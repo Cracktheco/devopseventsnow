917afe454ad9e2ab42b8f4c0ace7a082d221fc6b
Cracktheco <syedahmed29@gmail.com>
2019-08-23 14:39:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const port = 3000
 const controller = require('./controller/controller');
 app.get('/', (req, res) => res.send(controller.events))
 
+app.get('/newroute', (req, res) => res.send("hello new route"));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
